d972667cc92acd7675fd99b3dbed024a13dbbe6e
ravivats <vats.ravi96@gmail.com>
2017-05-09 19:49:25 +0530

    Live nasa video and gallery features added.

--- app/src/main/java/com/example/ravivats/moonwalk/GalleryActivity.java
+++ app/src/main/java/com/example/ravivats/moonwalk/GalleryActivity.java
@@ -1,16 +1,126 @@
 package com.example.ravivats.moonwalk;
 
+import android.app.ProgressDialog;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.ravivats.moonwalk.adapter.GalleryAdapter;
+import com.example.ravivats.moonwalk.app.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class GalleryActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private static final String jsonURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-2-3&api_key=DEMO_KEY";
+    private ArrayList<Image> images;
+    private ProgressDialog pDialog;
+    private GalleryAdapter mAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-        String code = getIntent().getStringExtra("name");
-        Toast.makeText(this, code, Toast.LENGTH_LONG).show();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        pDialog = new ProgressDialog(this);
+        images = new ArrayList<>();
+        mAdapter = new GalleryAdapter(getApplicationContext(), images);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        recyclerView.addOnItemTouchListener(new GalleryAdapter.RecyclerTouchListener(getApplicationContext(), recyclerView, new GalleryAdapter.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("images", images);
+                bundle.putInt("position", position);
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                SlideshowDialogFragment newFragment = SlideshowDialogFragment.newInstance();
+                newFragment.setArguments(bundle);
+                newFragment.show(ft, "slideshow");
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+        fetchImages();
+    }
+
+    private void fetchImages() {
+
+        pDialog.setMessage("Downloading json...");
+        pDialog.show();
+        RequestQueue rQueue = Volley.newRequestQueue(GalleryActivity.this);
+
+        JsonObjectRequest req = new JsonObjectRequest
+                (Request.Method.GET, jsonURL, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        pDialog.hide();
+                        images.clear();
+                        JSONArray response1=new JSONArray();
+                        try {
+                             response1=response.getJSONArray("photos");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        for (int i = 0; i < response1.length(); i++) {
+                            try {
+                                JSONObject object = response1.getJSONObject(i);
+                                Image image = new Image();
+                                image.setName(object.getString("id")+":"+object.getString("sol"));
+                                image.setSmall(object.getString("img_src"));
+                                image.setMedium(object.getString("img_src"));
+                                image.setLarge(object.getString("img_src"));
+                                image.setTimestamp(object.getString("earth_date"));
+                                images.add(image);
+
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                            }
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+
+                    }
+                });
+
+        // Adding request to request queue
+        rQueue.add(req);
     }
 }
