6c5929c7055041a358a3933494d9f8bd10ca5462
ravivats <vats.ravi96@gmail.com>
2017-05-13 16:09:19 +0530

    vats

--- app/src/main/java/com/example/ravivats/moonwalk/MainActivity.java
+++ app/src/main/java/com/example/ravivats/moonwalk/MainActivity.java
@@ -7,47 +7,68 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton galleryBtn,locationBtn, menuChoiceBtn,quizBtn, liveStreamBtn,aboutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+        galleryBtn =(ImageButton) findViewById(R.id.imageGallery);
+        locationBtn =(ImageButton) findViewById(R.id.imageLocation);
+        menuChoiceBtn =(ImageButton) findViewById(R.id.imageMoon);
+        quizBtn =(ImageButton) findViewById(R.id.imageQuiz);
+        liveStreamBtn =(ImageButton) findViewById(R.id.imageSettings);
+        aboutBtn =(ImageButton) findViewById(R.id.imageAbout);
 
-    public void fnGallery(View v)
-    {Intent my_intent = new Intent(this,GalleryActivity.class);
-        String s="Welcome to NASA Image Gallery";
-        my_intent.putExtra("name",s);
-        startActivity(my_intent);
-    }
-    public void fnLocation(View v)
-    {Intent my_intent1 = new Intent(this,LocationActivity.class);
-        String s="Welcome to Location Tracking page";
-        my_intent1.putExtra("name",s);
-        startActivity(my_intent1);
-    }
-    public void fnMoon(View v)
-    {Intent my_intent = new Intent(this,MenuActivity.class);
-        startActivity(my_intent);
-    }
-    public void fnQuiz(View v)
-    {Intent my_intent = new Intent(this,QuizActivity.class);
-        String s="Welcome to this Short Quiz.\n" +
-                "Test your knowledge by answering these questions. ";
-        my_intent.putExtra("name",s);
-        startActivity(my_intent);
-    }
-    public void fnSettings(View v)
-    {Intent my_intent = new Intent(this,LiveStreamActivity.class);
-        startActivity(my_intent);
-    }
-    public void fnAbout(View v)
-    {Intent my_intent = new Intent(this,AboutActivity.class);
-        startActivity(my_intent);
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent my_intent = new Intent(MainActivity.this,GalleryActivity.class);
+                String s="Welcome to NASA Image Gallery";
+                my_intent.putExtra("name",s);
+                startActivity(my_intent);
+            }
+        });
+        locationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent my_intent = new Intent(MainActivity.this,LocationActivity.class);
+                String s="Welcome to Location Tracking page";
+                my_intent.putExtra("name",s);
+                startActivity(my_intent);
+            }
+        });
+        menuChoiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+            }
+        });
+        quizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent my_intent = new Intent(MainActivity.this,QuizActivity.class);
+                String s="Welcome to the Quiz section";
+                my_intent.putExtra("name",s);
+                startActivity(my_intent);
+            }
+        });
+        liveStreamBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LiveStreamActivity.class));
+            }
+        });
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
     }
 
     @Override
